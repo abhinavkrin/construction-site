275c386046c3462274a8e33c8a88f32e7065393c
Abhinav Kumar <work.abhinavkr@gmail.com>
2020-11-24 00:18:38 +0530

    Added carousel

--- src/Components/Home/HomeCarousel.js
+++ src/Components/Home/HomeCarousel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import extBuildingThumb from '../../assets/images/ext_building_res_thumb.jpg';
 import welcomeThumb from '../../assets/images/welcome_thumb.jpg';
@@ -7,7 +7,7 @@ import roofingThumb from '../../assets/images/roofing_thumb.jpg';
 
 
 const carouselData = [{
-    title: "welcome",
+    title: "Welcome",
     body: "Benjamin Maintenance is located in Brooklyn New York and services the Greater New York City Metropolitan Area.  Benjamin Maintenance Corp is one of New York City’s leading contractors in the field of Exterior Building Restoration, Waterproofing Systems, Roofing Systems, and Plaza Deck Pavers.",
     image: welcomeThumb
 },{
@@ -24,6 +24,21 @@ const carouselData = [{
     image: portfolioThumb
 }]
 function HomeCarousel(){
+    
+    const [currentSlide,setCurrentSlide] = useState(0);
+
+    const next = () => {
+        if(carouselData.length){
+            setCurrentSlide((currentSlide + 1) % carouselData.length);
+        }
+    }; 
+
+    const prev = () => {
+        if(carouselData.length){
+            setCurrentSlide((carouselData.length + currentSlide - 1) % carouselData.length);
+        }
+    };
+
     const carouselItems = carouselData.map((item,i) => 
         <Carousel.Item key={item.title+i}>
             <img
@@ -32,13 +47,20 @@ function HomeCarousel(){
             alt="First slide"
             />
             <Carousel.Caption className="carousel-body">
-                <h3><span>{item.title}</span></h3>
-                <p>{item.body}</p>
+                <h3 className="slide-title"><span>{item.title}</span></h3>
+                <p className="slide-body">{item.body}</p>
+                <div className="controls">
+                    <div className="control-button" onClick={prev}>Previous</div>
+                    <div className="control-button" onClick={next}>Next</div>
+                </div>
             </Carousel.Caption>
         </Carousel.Item>
-        )
+        );
     return (
-    <Carousel>
+    <Carousel controls={false} 
+        onSelect={i=>setCurrentSlide(i)}
+        indicators={true}
+        activeIndex={currentSlide}>
         {carouselItems}            
     </Carousel>
     )
