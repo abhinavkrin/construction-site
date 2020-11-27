034feba91b1218edd7c5a34d1a3cb7f0a306a68d
Abhinav Kumar <work.abhinavkr@gmail.com>
2020-11-27 11:41:12 +0530

    self serve font-awesome

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './assets/styles/App.scss';
 import '@rmwc/icon/styles';
 import '@rmwc/icon-button/styles';
+import './assets/styles/fa/css/all.min.css'
 import MainComponent from './Components/MainComponent';
 import ScrollToTop from './Components/Common/ScrollToTop';
 
